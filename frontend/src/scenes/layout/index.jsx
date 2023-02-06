cd375f1b20a6c0a4f1960417db6ae9b78fada25b
imrankhakwani <30284761+imrankhakwani@users.noreply.github.com>
2023-02-07 00:04:26 +0500

    Frontend Setup

---
+++ frontend/src/scenes/layout/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Box, useMediaQuery } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navbar from 'components/Navbar';
+import SideBar from 'components/Sidebar';
+
+
+function Layout() {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
+      <SideBar
+        user = {{ name: "layla", occupation: "developer" }}
+        isNonMobile={isNonMobile}
+        drawerWidth="250px"
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
+      <Box>
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
+
+export default Layout
